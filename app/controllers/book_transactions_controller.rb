5ed31fbf3f7feacb9fdaeaa5ce6711374e48f0b5
= <hmgaar@gmail.com>
2017-03-01 03:03:49 -0500

    Added set_book, book_transaction_params and create methods to our BookTransactionController

--- app/controllers/book_transactions_controller.rb
+++ app/controllers/book_transactions_controller.rb
@@ -1,5 +1,27 @@
 class BookTransactionsController < ApplicationController
+  before_action :set_book, only:[:create]
+
+  def create
+    @book_transaction = @book.transactions.create(book_transaction_params)
+      if @book_transaction.transaction_type == "issued" && @book.book_quantity < 0 then
+        @book.quantity -= 1
+        @book_transaction.save!
+      elsif @book_transaction.transaction_type == "returned"
+        @book.quantity += 1
+        @book_transaction.save!
+      else
+        render json: {error: "No more Books Left"}
+    end
+  end
 
 
+  private
+  def book_transaction_params
+    params.require(:book_transaction).permit(:transaction_type)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 
 end
