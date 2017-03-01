03756540d982737c8f35e8fc7bdba8a13829112d
= <hmgaar@gmail.com>
2017-03-01 06:09:10 -0500

    BookTransactions are now being created and attached to Book_ID and you can Return a Book, by changing trans_status:false and trans_type:returned

--- app/controllers/book_transactions_controller.rb
+++ app/controllers/book_transactions_controller.rb
@@ -1,23 +1,27 @@
 class BookTransactionsController < ApplicationController
-  before_action :set_book, only:[:create]
+  before_action :set_book, only: [:create]
+  before_action :check_book_quantity, only: [:create]
 
   def create
-    @book_transaction = @book.transactions.create(book_transaction_params)
-      if @book_transaction.transaction_type == "issued" && @book.book_quantity < 0 then
-        @book.quantity -= 1
-        @book_transaction.save!
-      elsif @book_transaction.transaction_type == "returned"
-        @book.quantity += 1
-        @book_transaction.save!
-      else
-        render json: {error: "No more Books Left"}
-    end
+    transaction = @book.book_transactions.create
+    @book.book_quantity -= 1
+    @book.save!
+    transaction.save!
   end
 
+  def update
+    @book_transaction = BookTransaction.find(params[:id])
+    @book_transaction.update(transaction_type: 'returned', transaction_status:false)
+  end
 
   private
   def set_book
-    @book = Book.find(params[:id])
+    @book = Book.find(params[:book_id])
   end
 
+  def check_book_quantity
+    if @book.book_quantity < 1
+      render json: {'error': 'Cant create BookTransaction because Book.book_quantity < 1'}
+    end
+  end
 end
--- app/models/book_transaction.rb
+++ app/models/book_transaction.rb
@@ -1,14 +1,17 @@
 class BookTransaction < ApplicationRecord
   belongs_to :book
+  after_update :update_book_quantity
   after_initialize :set_default, :if => :new_record?
 
+
+  def update_book_quantity
+    @book = Book.find(self.book_id)
+    @book.book_quantity += 1
+    @book.save
+  end
+
   def set_default
-    @book = Book.find(id: self.book_id)
-    if @book.book_quantity < 0
       self.transaction_type = "issued"
       self.transaction_status = true
-    else
-      self.transaction_type = "unavailable"
-    end
   end
 end
