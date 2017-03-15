20a93c893dc41e048b873bcf721baadc493ec697
= <hmgaar@gmail.com>
2017-03-15 15:19:18 -0400

    delete un needed controller

--- app/controllers/static_controller.rb
+++
@@ -1,5 +0,0 @@
-class  StaticController < ApplicationController
-  def index
-    render :file => './client/dist/client/index.html'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root :to => 'static#index'
   resources :books do
     resources :book_transactions
-  end 
+  end
 end
