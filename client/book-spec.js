ccde5251f87856465b26c5c5a3ebb7a4976679fd
= <hmgaar@gmail.com>
2017-03-17 14:41:41 -0400

    added jasmine, protractor to the project

---
+++ client/book-spec.js
@@ -0,0 +1,22 @@
+describe('angularjs homepage todo list', function() {
+  it('should add a todo', function() {
+    // browser.get('http://localhost:3000');
+    // element(by.css('#971-1-44329-534-9')).click();
+
+    // element(by.css('#14')).click();
+    // element(by.css('#14')).click();
+    // element(by.css('#14')).click();
+    // element(by.css('#14')).click();
+
+    // for(var x=0; x<50; x++){
+    //   element(by.css('#next')).click();
+    // element(by.css('#971-1-44329-534-9')).click();
+    // }
+    // for(var x=0; x<5;x++){
+    //   element(by.css('#previous')).click();
+    // }
+    // for(var x=0; x<1;x++){
+    //   element(by.css('#14')).click();
+    // }
+  });
+});
